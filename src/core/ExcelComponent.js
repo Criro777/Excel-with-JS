5b2277a1695d4de1114b22a4b3fe2acc73e56274
Criro777 <coolalexser777@gmail.com>
2020-11-28 17:27:37 +0300

    Add & remove listeners to DOM

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,7 +1,22 @@
 import {DOMListener} from '@core/DOMListener';
 
 export class ExcelComponent extends DOMListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+  }
+
+  // Возвращает шаблон компонента
   toHTML() {
     return ''
+  }
+
+  // Инициализирует обработчики событий компонента
+  init() {
+    this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners()
   }
 }
