9bb3ea0946e76bef9a99a4892f457c90dc37b255
Criro777 <coolalexser777@gmail.com>
2020-11-29 01:21:51 +0300

    Table columns resize logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
+import {$} from '@/core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -17,7 +18,22 @@ export class Table extends ExcelComponent {
 
   onMousedown(event) {
     if (event.target.dataset.resize) {
-      console.log('Start resizing', event.target.dataset.resize)
+      const $target = $(event.target)
+      const $parent = $target.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+
+      document.onmousemove = e => {
+        const delta = e.pageX - coords.right
+        const value = coords.width + delta
+        $parent.$el.style.width = value + 'px'
+        cells.forEach(el => el.style.width = value + 'px')
+      }
+
+      document.onmouseup = () => {
+        document.onmousemove = null
+      }
     }
   }
 }
