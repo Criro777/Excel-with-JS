739247795a4242ab452af30a5f1c510682c5f0a0
Criro777 <coolalexser777@gmail.com>
2020-11-29 01:44:48 +0300

    Table rows resize logic & DOM class update

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -21,7 +21,7 @@ function toColumn(col, index) {
 function createRow(index, content) {
   const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-    <div class="row">
+    <div class="row" data-type="resizable">
       <div class="row-info">
         ${index ? index : ''}
         ${resize}
@@ -30,6 +30,7 @@ function createRow(index, content) {
     </div>
   `
 }
+
 function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
