7703febd0156e56d9f5e4bc8a1c6af6f2a0987f3
Criro777 <coolalexser777@gmail.com>
2021-02-20 23:41:24 +0300

    Binding excel components between themselves

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,9 +4,13 @@ export class ExcelComponent extends DOMListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscribers = []
+
     this.prepare()
   }
 
+  // Настраивает компонент до инициализации
   prepare() {}
 
   // Возвращает шаблон компонента
@@ -14,12 +18,27 @@ export class ExcelComponent extends DOMListener {
     return ''
   }
 
-  // Инициализирует обработчики событий компонента
+  // Уведомление слушателей о событии event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Подписка на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // Инициализация обработчиков событий компонента
+  // Добавление DOM слушателей
   init() {
     this.initDOMListeners()
   }
 
+  // Удаление компонент
+  // Очистка слушателей
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
