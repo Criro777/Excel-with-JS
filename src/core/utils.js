ef6dbfd0d37115e8dd84be7394b9729412b2866d
Criro777 <coolalexser777@gmail.com>
2021-02-22 01:24:39 +0300

    Saving & rendering changed cells styles

--- src/core/utils.js
+++ src/core/utils.js
@@ -33,4 +33,10 @@ export function camelToDashCase(str) {
   return str.replace(/([A-Z])/g, g => `-${g[0].toLowerCase()}`)
 }
 
+export function toInlineStyles(styles = {}) {
+  return Object.keys(styles)
+      .map(key => `${camelToDashCase(key)}: ${styles[key]}`)
+      .join(';')
+}
+
 
